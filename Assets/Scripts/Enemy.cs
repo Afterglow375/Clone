30722bf7c09e1808a29c80f5ea0e0087a60d76a7
Alex Tatusko <afterglow376@gmail.com>
2023-07-26 15:54:28 -0400

    Added enemy animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,28 +1,32 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] private Rigidbody2D _rb;
-    [SerializeField] private Vector2 _movement;
+    [SerializeField] private float _moveSpeed;
     [SerializeField] private Animator _animator;
-    private readonly Vector3 _leftScale = new Vector3(-1, 1, 1); 
-    private static readonly int Speed = Animator.StringToHash("speed");
+    private readonly Vector3 _leftScale = new Vector3(-1, 1, 1);
+    private Transform _player;
+    private Vector2 _movement;
     private static readonly int Horizontal = Animator.StringToHash("horizontal");
+    private static readonly int Speed = Animator.StringToHash("speed");
 
-    void Update()
+    void Start()
     {
-        _movement.x = Input.GetAxis("Horizontal");
-        _movement.y = Input.GetAxis("Vertical");
+        _player = GameObject.Find("Player").transform;
+    }
 
+    private void Update()
+    {
+        _movement = (_player.position - transform.position).normalized;
         _animator.SetFloat(Horizontal, _movement.x);
         _animator.SetFloat(Speed, _movement.sqrMagnitude);
         
-        if (_movement.x < 0)
+        if (_movement.x > 0)
         {
             transform.localScale = _leftScale;
         }
-        else if (_movement.x > 0)
+        else if (_movement.x < 0)
         {
             transform.localScale = Vector3.one;
         }
