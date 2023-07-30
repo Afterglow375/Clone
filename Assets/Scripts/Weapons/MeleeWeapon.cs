e666e358e5cc8e757c5afe6513498bc5572f82b3
Alex Tatusko <afterglow376@gmail.com>
2023-07-30 18:10:46 -0400

    Melee weapon does damage now

--- Assets/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Weapons
 {
@@ -7,9 +8,33 @@ namespace Weapons
         [SerializeField] private Animator _animator;
         private static readonly int AttackAnim = Animator.StringToHash("Attack");
 
+        public Transform circleOrigin;
+        public float radius;
+
         protected override void AttackImpl()
         {
             _animator.SetTrigger(AttackAnim);
+        }
+        
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Vector3 position = circleOrigin == null ? Vector3.zero : circleOrigin.position;
+            Gizmos.DrawWireSphere(position, radius);
+        }
+
+        // Called by attack animation
+        public void DetectColliders()
+        {
+            LayerMask mask = LayerMask.GetMask("EnemyHitbox");
+            foreach (Collider2D col in Physics2D.OverlapCircleAll(circleOrigin.position, radius, mask))
+            {
+                if (col.CompareTag("Enemy"))
+                {
+                    Enemy enemy = col.transform.parent.GetComponent<Enemy>();
+                    enemy.TakeDamage(attackDamage);
+                }
+            }
         }
     }
 }
